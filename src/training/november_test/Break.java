eaf19962702b1f99f263d52b4417858264179df6
paquet04 <sushitabetai3@gmail.com>
2021-11-10 09:43:00 +0900

    2021110_src/training/november_test/を追加

---
+++ src/training/november_test/Break.java
@@ -0,0 +1,44 @@
+package training.november_test;
+
+class Break {
+    public static void main(String[] args) {
+        //iが5の倍数なら処理を中断、他はprintlnで表示する
+        System.out.println("=========== 処理を開始します ===========");
+        for (int i = 1; i < 10; i++){
+            if (i % 5 == 0){
+                System.out.println("=== i = " + i +"のためbreak処理を実行します ===");
+                break;   //breakはforないしwhileからぬける
+            }
+            System.out.println("i = " + i);
+        }
+        System.out.println("================== End ==================\n");
+
+
+        //iが8の倍数なら処理を中断、他はprintlnで表示する
+        System.out.println("=========== 処理を開始します ===========");
+        int i = 1;
+        while(i <= 10){
+            if(i % 8 == 0 ){
+                System.out.println("=== i = " + i +"のためbreak処理を実行します ===");
+                break;
+            }
+            System.out.println("i = " + i);
+            i++;
+        }
+        System.out.println("================== End ==================\n");
+
+        //9の倍数
+        System.out.println("=========== 処理を開始します ===========");
+        i = 1;
+        do{
+            if(i % 9 == 0 ){
+                System.out.println("=== i = " + i +"のためbreak処理を実行します ===");
+                break;
+            }
+            System.out.println("i = " + i);
+            i++;
+        }while(i <= 10);
+        System.out.println("================== End ==================");
+    }
+}
+
--- src/training/november_test/Continue.java
+++ src/training/november_test/Continue.java
@@ -1,14 +1,45 @@
 package training.november_test;
 
-public class Continue {
+class Continue {
     public static void main(String[] args) {
         //iが3の倍数ならスルーし、他はprintlnで表示する
+        System.out.println("=========== 処理を開始します ===========");
         for (int i = 1; i < 10; i++){
             if (i % 3 == 0){
+                System.out.println("=== i = " + i +" のためcontinue処理を実行します ===");
                 continue;   //continueはprintlnをスルーしてforの最初に戻る命令
             }
             System.out.println("i = " + i);
         }
-        System.out.println("End");
+        System.out.println("==================== End ====================\n");
+
+        //iが7の倍数ならスルーし、他はprintlnで表示する
+        System.out.println("=========== 処理を開始します ===========");
+        int i = 1;
+        while(i <= 15){
+            if (i % 7 == 0){
+                System.out.println("=== i = " + i +" のためcontinue処理を実行します ===");
+                i = i + 1;
+                continue;   //continueはprintlnをスルーしてforの最初に戻る命令
+            }
+            System.out.println("i = " + i);
+            i = i + 1;
+        }
+        System.out.println("==================== End ====================\n");
+
+        //iが6の倍数ならスルーし、他はprintlnで表示する
+        System.out.println("=========== 処理を開始します ===========");
+        i=1;
+        do{
+            if (i % 6 == 0){
+                System.out.println("=== i = " + i +" のためcontinue処理を実行します ===");
+                i = i + 1;
+                continue;   //continueはprintlnをスルーしてforの最初に戻る命令
+            }
+            System.out.println("i = " + i);
+            i = i + 1;
+        }while(i <= 15);
+        System.out.println("==================== End ====================\n");
+
     }
 }
