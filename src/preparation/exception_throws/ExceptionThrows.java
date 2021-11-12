28cbf7eaf99e687378b8f0e2e1a37399f317fd58
paquet04 <sushitabetai3@gmail.com>
2021-11-12 15:24:03 +0900

    2021112_src/preparation/exception_throws 作成

---
+++ src/preparation/exception_throws/ExceptionThrows.java
@@ -0,0 +1,37 @@
+package preparation.exception_throws;
+
+//スロー
+class ExceptionThrows {
+    public static void main(String[] args) {
+
+        System.out.println();
+        System.out.println("==================== 開始 ====================");
+        try {
+            ThrowsTest throwsTest = new ThrowsTest();
+            throwsTest.sample();
+        }catch (Exception e){
+            System.out.println("------------------- catch -------------------");
+            System.out.println("e.getCause() : " + e.getCause());     //null
+            System.out.println("e.getMessage() : " + e.getMessage()); //null
+            e.printStackTrace();
+        } finally {
+            System.out.println("==================== 終了 ====================\n");
+        }
+
+
+        System.out.println("==================== 開始 ====================");
+        try {
+            ThrowsTest throwsTest = new ThrowsTest();
+            System.out.println("sample2(return Exception) 実行");
+            Exception exception = throwsTest.sample2();
+            System.out.println(exception); //java.lang.Exceptionが返ってきた
+        }catch (Exception e){
+            System.out.println("------------------- catch -------------------");
+            System.out.println("e.getCause() : " + e.getCause());
+            System.out.println("e.getMessage() : " + e.getMessage());
+        } finally {
+            System.out.println("==================== 終了 ====================");
+        }
+        System.out.println("\n==================== 完了 ====================");
+    }
+}
---
+++ src/preparation/exception_throws/ThrowsTest.java
@@ -0,0 +1,18 @@
+package preparation.exception_throws;
+
+class ThrowsTest {
+
+    public ThrowsTest() {
+    }
+
+    //ただ例外をスローするだけ
+    public void sample() throws Exception {
+        System.out.println("-------- throwsTest.sample()実行開始 --------");
+        throw new Exception();
+    }
+
+    //例外をリターンするだけ
+    public Exception sample2(){
+        return new Exception();
+    }
+}
