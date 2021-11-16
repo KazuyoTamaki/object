494da09f8814283214c0d3e7b74a738bd4e18a99
paquet04 <sushitabetai3@gmail.com>
2021-11-16 09:39:22 +0900

    2021116_src/preparation/preparation/dateTimeAPI/DateTimeAPISample 更新

--- src/preparation/dateTimeAPI/DateTimeAPISample.java
+++ src/preparation/dateTimeAPI/DateTimeAPISample.java
@@ -37,9 +37,14 @@ class DateTimeAPISample {
 
         //計算
         System.out.println("---------- 加算減算 ----------");
-        System.out.println("LocalDate plusDay20 : " + date1.plusDays(20));
+        System.out.println("LocalDate Normal     : " + date1);
+        System.out.println("LocalDate plusDay20  : " + date1.plusDays(20));
         System.out.println("LocalDate minusYear3 : " + date1.minusYears(3));
 
+        System.out.println("-- 秒 --");
+        System.out.println("LocalTime Nanosecond Normal      : " + time1);
+        System.out.println("LocalTime plusNanoSecond 50,000  : " + time1.plusNanos(50_000));
+        System.out.println("LocalTime plusNanoSecond 20,000  : " + time1.minusNanos(20_000));
 
         System.out.println("======================= 完了 =======================");
     }
