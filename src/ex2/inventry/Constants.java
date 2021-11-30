0455e0f44f388709a2da8e3d0e8f6e8171104fdf
paquet04 <sushitabetai3@gmail.com>
2021-11-30 11:40:40 +0900

    20211130_ex2追加

---
+++ src/ex2/inventry/Constants.java
@@ -0,0 +1,13 @@
+package ex2.inventry;
+
+class Constants {
+    //OSの種類
+    public static final String WINDOWS_10 = "windows10";
+    public static final String WINDOWS_11 = "windows11";
+    public static final String MAC = "Mac";
+
+    //OSのビット マックにも32、64bitある
+    public static final int BIT_64 = 64;
+    public static final int BIT_32 = 32;
+
+}
---
+++ src/ex2/inventry/ViewStock.java
@@ -0,0 +1,56 @@
+package ex2.inventry;
+
+import java.util.*;
+
+//初期定義と基本的に金額を引き出すクラス
+class ViewStock {
+    public static void main(String[] args) {
+        List<PersonalComputer> pcList = new ArrayList<>();
+
+        pcList.addAll(Arrays.asList(
+                new PersonalComputer("やまだ", 1, 1, 40_000, Constants.BIT_64, Constants.WINDOWS_10),
+                new PersonalComputer("いけだ", 2, 1, 90_000, Constants.BIT_64, Constants.WINDOWS_10),
+                new PersonalComputer("いのうえ", 3, 1, 80_000, Constants.BIT_64, Constants.WINDOWS_10),
+                new PersonalComputer("たむら", 4, 2, 120_000, Constants.BIT_32, Constants.WINDOWS_10),
+                new PersonalComputer("わだ", 5, 2, 30_000, Constants.BIT_32, Constants.WINDOWS_10),
+                new PersonalComputer("くどう", 6, 2, 150_000, Constants.BIT_64, Constants.WINDOWS_10),
+                new PersonalComputer("さらど", 7, 3, 40_000, Constants.BIT_32, Constants.WINDOWS_10),
+                new PersonalComputer("あいざわ", 8, 3, 70_000, Constants.BIT_32, Constants.WINDOWS_10)
+        ));
+
+
+        //倉庫No:Int 価格:Int
+        Map<Integer, ArrayList<Integer>> storagePriceMap = new HashMap<>();
+
+        //リスト内 ここは上の初期化で実行されてるので、おそらくは実行されない。
+        for (PersonalComputer p : pcList) {
+            int storageNo = p.getStorageNo(); //何度も使用するため変数に格納する
+            if (!storagePriceMap.containsValue(storageNo)) {          //storagePriceMapにstorageNoがない場合
+                storagePriceMap.put(storageNo, new ArrayList<>());  //新しい倉庫Noをつくる
+            }                                                       //万が一一致したら上書きを行うため、念のためチェック
+            storagePriceMap.get(storageNo).add(p.getPrice());  //価格を追加
+        }
+
+        //デバッグ
+        System.out.println("StoragePriceMapの中身確認");
+        for (Map.Entry entry : storagePriceMap.entrySet()) {
+            System.out.print("倉庫番号 " + entry.getKey() + ":");
+            System.out.print(entry.getValue() + "\n");
+        }
+
+
+        //ここから勝手に作る
+        /*Map<Integer,List<PersonalComputer>> computerMap = new HashMap<>();
+
+        for(PersonalComputer pc: pcList){
+            if(!(computerMap.containsKey(pc.getUserName())) ){
+                List<pcList> list = new ArrayList();
+                list.add(pc);
+                computerMap.put(pc.getUserName(), list);
+            }
+            else{
+                computerMap.get(pc.getUserName()).add(pc);
+            }
+        }*/
+    }
+}
